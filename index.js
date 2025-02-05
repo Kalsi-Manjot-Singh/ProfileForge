c18c79551b1e3470831271eb156b688ec206b49c
Kalsi-Manjot-Singh <kalsimanjotsingh00@gmail.com>
2025-02-05 17:15:21 +0530

    Refactored to express js with middlewares and error handling

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-const http = require("http");
+// External library imports
 const axios = require("axios");
+const express = require("express");
 
-const server = http.createServer( async (req,res)=>{ 
-  if(req.url === "/profiles" && req.method === "GET") {
-    try {
-      res.writeHead(200, { "Content-Type": "application/json"});
-      const apiResponse = await axios.get("http://localhost:50325/api/v1/user/list?page=1&page_size=50")
-      res.end(JSON.stringify(apiResponse.data));
-    } catch(err) {
-        res.writeHead(500, { "Content-Type": "application/json"});
-        res.end(JSON.stringify({error: "Failed to fetch profiles", details: err.message}));
-      }
-    }
-    else {
-      res.writeHead(404, { "Content-Type": "text/plain"});
-      res.write("Route not found");
-      res.end();
+const app = express();
+const PORT = 3000; // Port for the server to listen to.
+
+app.use(express.json()); // middleware to parse req body
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/profiles", async (req, res)=>{
+  try {
+    const apiResponse = await axios.get("http://localhost:50325/api/v1/user/list?page=1&page_size=50")
+    res.status(200).json(apiResponse.data);
+  } catch(err) {
+      res.status(500).json({error: "Failed to fetch profiles"});
+      console.error(`API error: ${err.message}`);
     }
 })
-const port = 3000;
-server.listen(port);
-console.log("Server listening on port 3000...")
+
+// Handling unsupported methods
+app.all("/profiles", (req,res)=>{
+  res.status(405).json({error: "Method not allowed"});
+})
+
+// Handling undefined routes
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+app.listen(PORT, ()=> console.log("Server listening on port 3000..."));
