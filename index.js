5872a7cc5276f48cb27212d1a46486c1cc802472
Kalsi-Manjot-Singh <kalsimanjotsingh00@gmail.com>
2025-02-06 06:51:45 +0530

    Added timestamp in middleware for better debugging and improved error consistency

--- index.js
+++ index.js
@@ -5,31 +5,34 @@ const express = require("express");
 const app = express();
 const PORT = 3000; // Port for the server to listen to.
 
-app.use(express.json()); // middleware to parse req body
-// Logging middleware
+// Middleware
+app.use(express.json()); 
 app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+  console.log(` ${new Date().toISOString()} - ${req.method} ${req.url}`);
   next();
 });
 
-app.get("/profiles", async (req, res)=>{
+// Routes
+app.get("/profiles", async (req, res) => {
   try {
-    const apiResponse = await axios.get("http://localhost:50325/api/v1/user/list?page=1&page_size=50")
+    const apiResponse = await axios.get(
+      "http://localhost:50325/api/v1/user/list?page=1&page_size=50"
+    );
     res.status(200).json(apiResponse.data);
-  } catch(err) {
-      res.status(500).json({error: "Failed to fetch profiles"});
-      console.error(`API error: ${err.message}`);
-    }
-})
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch profiles", code: "API error" });
+    console.error(`API error: ${err.message}`);
+  }
+});
 
 // Handling unsupported methods
-app.all("/profiles", (req,res)=>{
-  res.status(405).json({error: "Method not allowed"});
-})
+app.all("/profiles", (req, res) => {
+  res.status(405).json({ error: "Method not allowed", code: 405 });
+});
 
 // Handling undefined routes
 app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
+  res.status(404).json({ error: "Route not found", code: 404 });
 });
 
-app.listen(PORT, ()=> console.log("Server listening on port 3000..."));
+app.listen(PORT, () => console.log("Server listening on port 3000..."));
