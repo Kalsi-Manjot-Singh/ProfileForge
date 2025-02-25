0b677c910a0aa5dcd170e438df73f42c5fa94c4b
Kalsi-Manjot-Singh <kalsimanjotsingh00@gmail.com>
2025-02-25 17:28:11 +0530

    taking user input from a file and using it to create profiles.json to send to use in API payload

---
+++ profileJsonGen.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+
+async function generateProfilesJson(){
+  try{
+    // An array of file paths
+  const filePaths = [
+    "./userProvidedData/names.txt",
+    "./userProvidedData/location.txt",
+    "./userProvidedData/proxy.txt",
+    "./userProvidedData/longitude.txt",
+    "./userProvidedData/latitude.txt"
+  ];
+
+  // An array of promises that read the files
+  const fileReadPromises = filePaths.map(path => fs.promises.readFile(path, "utf-8"));
+  
+  // Reading all the files concurently using Promises.all
+  const fileContents = await Promise.all(fileReadPromises);
+
+  // Processing each files content
+  const processedData = fileContents.map((fileData) => {
+    return fileData.split("\n").reduce( (acc,line) => {
+      const trimmed = line.trim();
+      if (trimmed) acc.push(trimmed);
+      return acc;
+    }, []);
+  });
+
+  // Destructuring the processedData array
+  const [names,locations,proxies,longitudes,latitudes] = processedData;
+
+  // Creating an array of profiles object using the destructed variables from processedData\
+  const profiles = [];
+
+  // Calculating total number of profiles
+  const totalProfiles = Math.min(
+    names.length,
+    locations.length,
+    proxies.length,
+    longitudes.length,
+    latitudes.length
+  );
+  
+  // Populating the profiles array from processedData
+  for(let i = 0; i < totalProfiles; i++) {
+    profiles.push({
+      name: names[i],
+      location: locations[i],
+      proxy: proxies[i],
+      longitude: longitudes[i],
+      latitude: latitudes[i],
+    });
+  }
+
+  // Writing to profiles.json
+  await fs.promises.writeFile("./profiles.json", JSON.stringify(profiles, null, 2), "utf-8");
+  console.log("Profiles successfully saved to profiles.json!");
+
+  } catch(error){
+    console.error("Error creating profiles.json ", error);
+  }
+
+}
+
+generateProfilesJson();
